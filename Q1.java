f645124dd0f9893add7a24e6d1d0b2fb70de4ae3
AKASH BHUJABAL <akashbhujabaliter@gmail.com>
2025-02-25 15:54:25 +0530

    Add files via upload
    Q1 updated

---
+++ Q1.java
@@ -0,0 +1,17 @@
+package basics_Java;
+
+public class Q1 
+{
+    public static void main(String[] args) 
+    {
+        String p_Name="Virat Kohli";
+        long acc_No=18087473849484l;
+        double p_Balance=123287374.123;
+        String acc_Type="Saving";
+        long mob_No=8976590876l;
+        String bank_Name="State Bank of India";
+
+        System.out.println("Player name is "+p_Name+".His account number is "+acc_No+".His total balance in this Bank account is "+p_Balance+".His account type is "+acc_Type+".His mobile number is "+mob_No+".His Bank name is "+bank_Name+"." );   
+    }
+}
+
