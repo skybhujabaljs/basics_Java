6468c0f04b2644ed592d5d857bc1f63493882e9f
AKASH BHUJABAL <akashbhujabaliter@gmail.com>
2025-02-25 16:29:42 +0530

    Update Q2.java

--- Q2.java
+++ Q2.java
@@ -11,3 +11,8 @@ public class Q2
         System.out.println("The BMI of the student "+s_name+" with weight "+s_Weight+ " and height "+s_Height+" is "+BMI+".");
     }
 }
+
+
+
+output:-
+    The BMI of the student Akash with weight 60.5 and height 150.5 is 0.0026710521958918775.
